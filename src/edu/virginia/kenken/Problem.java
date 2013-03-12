30d9b884965b868ea9661905cc02a7856375ce42
Art Chaidarun <artchaidarun@gmail.com>
2013-03-11 22:26:18 -0400

    Create classes

---
+++ src/edu/virginia/kenken/Problem.java
@@ -0,0 +1,13 @@
+package edu.virginia.kenken;
+
+public class Problem {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
