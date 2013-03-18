801bdcbbfef5056af2e94cbefd46ab11cba137b5
scteps <sc.teps92@gmail.com>
2013-03-18 00:19:23 -0400

    Add code to randomly insert double cell cages; need to fill in remaining board with legal cages with more than two cells per cage

--- src/edu/virginia/kenken/Problem.java
+++ src/edu/virginia/kenken/Problem.java
@@ -2,10 +2,14 @@ package edu.virginia.kenken;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class Problem {
 
   private final int size;
+  
+  // specifies number of double cell cages
+  private final int numDoubleBlocks = 20;
 
   public Problem(int size) {
     this.size = size;
@@ -48,8 +52,74 @@ public class Problem {
       }
       System.out.print("\n");
     }
+    
+    // creation of cages (only two cell blocks)
+    // TODO Fill in the rest of the board with legal cages (>2 cells each)
+    Random rand = new Random();
+    ArrayList<ArrayList<Integer>> grid = new ArrayList<ArrayList<Integer>>(size);
+    
+    // initialize the arrayList
+    for(int i = 0; i < size; ++i)
+    {
+      grid.add(new ArrayList<Integer>(Collections.nCopies(size, 0)));
+    }
+    
+    int blockCount = 1;   // keeps tracks of different cages in the grid
+    while (blockCount <= numDoubleBlocks)
+    {
+            
+      // random vertical or horizontal insertion
+      int currCell = rand.nextInt(size*size);
+      while(grid.get(currCell/size).get(currCell%size) != 0)
+      {
+        currCell = rand.nextInt(size*size);
+      }
+      if(rand.nextBoolean()) {
+        while(currCell%size == 0)           // don't want left edge
+        {
+          currCell = rand.nextInt(size*size);
+        }
+        if(grid.get(currCell/size).get(currCell%size - 1) == 0)
+        {
+          grid.get(currCell/size).set(currCell%size, blockCount);
+          grid.get(currCell/size).set(currCell%size - 1, blockCount);
+          ++blockCount;
+        }
+        else
+        {
+          continue;
+        }
+      }
+      else
+      {
+        while(currCell/size == size - 1)    // don't want bottom row
+        {
+          currCell = rand.nextInt(size*size);
+        }
+        if(grid.get(currCell/size + 1).get(currCell%size) == 0)
+        {
+          grid.get(currCell/size).set(currCell%size, blockCount);
+          grid.get(currCell/size + 1).set(currCell%size, blockCount);
+          ++blockCount;
+        }
+        else
+        {
+          continue;
+        }
+      }
+    }
+    
+    System.out.println("CAGES -- INCOMPLETE");
+    for(int i = 0; i < size; ++i)
+    {
+      for(int j = 0; j < size; ++j)
+      {
+        System.out.print(grid.get(i).get(j)+"\t");
+      }
+      System.out.println();
+    }
   }
-
+  
   public int getSize() {
     return size;
   }
