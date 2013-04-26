05fef7cf1c1fe322da9579f469012f6f7d435763
scteps <sc.teps92@gmail.com>
2013-04-26 10:09:10 -0400

    Implemented brute force solver (not tested)

--- src/edu/virginia/kenken/Solver.java
+++ src/edu/virginia/kenken/Solver.java
@@ -1,5 +1,56 @@
 package edu.virginia.kenken;
 
-public class Solver {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class Solver {
+  private Problem problem;
+  private ArrayList<ArrayList<Integer>> solution;
+  private boolean isSolved;
+
+  public Solver(Problem problem) {
+    this.problem = problem;
+    solution = new ArrayList<ArrayList<Integer>>();
+  }
+
+  public void solve() {
+    if (isSolved) {
+      System.out.println("The board has already been solved.");
+      return;
+    }
+
+    // Use the same shuffling algorithm that was used to create legal grids
+    while (!problem.checkGrid(solution)) {
+      for (int i = 0; i < problem.getSize(); ++i) {
+        solution.add(new ArrayList<Integer>());
+        for (int j = 0; j < problem.getSize(); ++j) {
+          solution.get(i).add((i + j) % problem.getSize() + 1);
+        }
+      }
+
+      // Shuffle rows
+
+      Collections.shuffle(solution);
+
+      // Transpose board matrix
+
+      int tmp;
+      for (int i = 0; i < problem.getSize(); ++i) {
+        for (int j = 0; j < i; ++j) {
+          tmp = solution.get(i).get(j);
+          solution.get(i).set(j, solution.get(j).get(i));
+          solution.get(j).set(i, tmp);
+        }
+      }
+
+      // Shuffle rows (which were the columns before transposition) again
+
+      Collections.shuffle(solution);
+    }
+    isSolved = true;
+  }
+
+  public ArrayList<ArrayList<Integer>> getSolution() {
+    return solution;
+  }
 }
